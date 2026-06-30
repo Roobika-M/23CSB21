095bd0ae03c55ef335c3b89f62353d07647270f0
Roobika <153609390+Roobika-M@users.noreply.github.com>
2026-06-30 13:00:04 +0530

    final

--- notification-app-be/notify/src/main/java/com/example/notify/NotificationController.java
+++ notification-app-be/notify/src/main/java/com/example/notify/NotificationController.java
@@ -1,5 +1,8 @@
 package com.example.notify;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
